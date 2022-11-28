fe4402f555206ae796413282215f1849f8655f73
Sabbi108 <66543725+Sabbi108@users.noreply.github.com>
2022-11-28 13:56:37 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const Bot = require("ctk-musicbot");
+
+const musicbot = new Bot({
+  Token: "NzQyMzk2NzE1NjI5NTQzNDk1.XzFg0g.bQexwnlsJysoALY1mEjP35MbUVk",
+  prefix: "#"
+});
+
+musicbot.start();
+
